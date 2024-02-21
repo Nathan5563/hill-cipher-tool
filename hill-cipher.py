49dc01cdda5eb662c7569f6ff022fd40e45abd12
Nathan <ndawit611@gmail.com>
2024-02-21 13:40:57 +0300

    Encryption now works for messages of any size with a 3x3 key

--- hill-cipher.py
+++ hill-cipher.py
@@ -10,29 +10,50 @@ def num_to_letter(num):
     letter = alphabet[num]
     return letter
 
-# Encrypt function currently only works with a 9 letter key and a 3 letter plainText
+# Encrypt function currently only works with n=3 (9 letter key)
 def encrypt(plainText, key):
+    # Defining variables
     keyArray = np.array([])
     plainTextArray = np.array([])
+    splitby = []
+    looprange = 1
     cipherText = ''
 
+    # Convert key to 3x3 matrix
     for i in key:
-        keyArray = np.append(keyArray, [letter_to_num(i)])
-                
+        keyArray = np.append(keyArray, [letter_to_num(i)])     
     keyMatrix = keyArray.reshape(3, 3)
-    
+
+    # Convert plain text to an array
     for i in plainText:
         plainTextArray = np.append(plainTextArray, [letter_to_num(i)])
+
+    # Find the indices at which the plain text array should be split to form 3x1 matrices
+    for i in range(1, len(plainTextArray)):
+        if i%3==0:
+            splitby.append(i)
+            looprange += 1
+            
+    # Split the plain text array into one or more 1x3 arrays
+    splitarr = np.split(plainTextArray, splitby)
+    
+    # To convert the 1x3 arrays into 3x1 matrices, first check if all arrays are full (i.e., have 3 elements)
+    # If the last array is missing elements, find how many and add in the letter 'Z'(number 25) as a filler
+    leftoverDifference = 3-len(splitarr[-1])
+    for i in range(leftoverDifference):
+        splitarr[-1] = np.append(splitarr[-1], [25])
+            
+    # Perform matrix multiplication and modulo 26 a number of times equal to the number of matrices
+    for i in range(looprange):
+        plainTextMatrix = splitarr[i].reshape(3, 1)
+        keyTextProduct = np.matmul(keyMatrix, plainTextMatrix)
+        cipherTextMatrix = keyTextProduct % 26
         
-    plainTextMatrix = plainTextArray.reshape(3, 1)
-    
-    keyTextProduct = np.matmul(keyMatrix, plainTextMatrix)
-    
-    cipherTextMatrix = keyTextProduct % 26
-    
-    cipherTextArray = np.squeeze(cipherTextMatrix)
-    
-    for i in cipherTextArray:
-        cipherText += f'{num_to_letter(int(i))}'
+        # Convert the resulting 3x1 matrix back into a 1x3 array
+        cipherTextArray = np.squeeze(cipherTextMatrix)
+        
+        # Convert the 1x3 array into cipher text
+        for i in cipherTextArray:
+            cipherText += f'{num_to_letter(int(i))}'
             
     return cipherText
