b3aba5a5bb2699687747e14c9c033668f3699414
Nathan <ndawit611@gmail.com>
2024-02-22 21:23:11 +0300

    Added buggy decrypt: index starting from 1??

--- hill-cipher.py
+++ hill-cipher.py
@@ -30,7 +30,7 @@ def encrypt(plainText, key):
 
     # Find the indices at which the plain text array should be split to form 3x1 matrices
     for i in range(1, len(plainTextArray)):
-        if i%3==0:
+        if i % 3 == 0:
             splitby.append(i)
             looprange += 1
             
@@ -57,3 +57,68 @@ def encrypt(plainText, key):
             cipherText += f'{num_to_letter(int(i))}'
             
     return cipherText
+
+def decrypt(cipherText, key):
+    # Defining variables
+    keyArray = np.array([])
+    cipherTextArray = np.array([])
+    splitby = []
+    looprange = 1
+    plainText = ''
+    
+    # Convert key to 3x3 matrix
+    for i in key:
+        keyArray = np.append(keyArray, [letter_to_num(i)])     
+    keyMatrix = keyArray.reshape(3, 3)
+        
+    modDet = np.linalg.det(keyMatrix) % 26
+    
+    adj = (np.linalg.inv(keyMatrix) * np.linalg.det(keyMatrix)) % 26
+    
+    inverseKeyMatrix = (modDet * adj) % 26
+    
+    print(modDet) 
+    print(inverseKeyMatrix)
+    
+    # Convert cipher text to an array
+    for i in cipherText:
+        cipherTextArray = np.append(cipherTextArray, [letter_to_num(i)])
+                
+    # Find the indices at which the cipher text array should be split to form 3x1 matrices
+    for i in range(1, len(cipherTextArray)):
+        if i % 3 == 0:
+            splitby.append(i)
+            looprange += 1
+        
+    # Split the cipher text array into one or more 1x3 arrays
+    splitarr = np.split(cipherTextArray, splitby)
+    
+    # To convert the 1x3 arrays into 3x1 matrices, first check if all arrays are full (i.e., have 3 elements)
+    # If the last array is missing elements, find how many and add in the letter 'Z'(number 25) as a filler
+    leftoverDifference = 3-len(splitarr[-1])
+    for i in range(leftoverDifference):
+        splitarr[-1] = np.append(splitarr[-1], [25])
+        
+    for i in range(looprange):
+        cipherTextMatrix = splitarr[i].reshape(3, 1)
+                
+        keyTextProduct = np.matmul(inverseKeyMatrix, cipherTextMatrix)
+        
+        plainTextMatrix = keyTextProduct % 26
+        
+        print(plainTextMatrix)
+        
+        # Convert the resulting 3x1 matrix back into a 1x3 array
+        plainTextArray = np.squeeze(plainTextMatrix)
+        
+        # Convert the 1x3 array into plain text
+        for i in plainTextArray:
+            plainText += f'{num_to_letter(int(i))}'
+            
+    return plainText
+
+encrypt('ACT', 'GYBNQKURP')
+# Output: POH
+
+decrypt('POH', 'GYBNQKURP')
+# Output: ACT
