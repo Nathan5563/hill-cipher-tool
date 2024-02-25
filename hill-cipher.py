45d3041711f9fde9d6bdf29c28a9e4c3aa074434
Nathan <ndawit611@gmail.com>
2024-02-25 12:11:32 +0300

    Moving duplicate code into separate functions

--- hill-cipher.py
+++ hill-cipher.py
@@ -13,118 +13,96 @@ def num_to_letter(num):
     return letter
 
 
-# Encrypt function currently only works with n=3 (9 letter key)
-def encrypt(plaintext, key):
-    # Defining variables
+# Currently only works with a 3x3 (9 letter) key
+def key_to_matrix(key):
     key_array = np.array([])
-    plain_text_array = np.array([])
-    split_by = []
-    loop_range = 1
-    cipherText = ''
 
-    # Convert key to 3x3 matrix
     for i in key:
         key_array = np.append(key_array, [letter_to_num(i)])
-    keyMatrix = key_array.reshape(3, 3)
+    key_matrix = key_array.reshape(3, 3)
 
-    # Convert plain text to an array
-    for i in plaintext:
-        plain_text_array = np.append(plain_text_array, [letter_to_num(i)])
+    return key_matrix
 
-    # Find the indices at which the plain text array should be split to form 3x1 matrices
-    for i in range(1, len(plain_text_array)):
+
+def string_to_matrix(string):
+    string_array = np.array([])
+    split_at = []
+    loop_range = 1
+
+    for i in string:
+        string_array = np.append(string_array, [letter_to_num(i)])
+
+    # Find the indices at which the array should be split to form 1x3 arrays
+    for i in range(1, len(string_array)):
         if i % 3 == 0:
-            split_by.append(i)
+            split_at.append(i)
             loop_range += 1
 
-    # Split the plain text array into one or more 1x3 arrays
-    splitarr = np.split(plain_text_array, split_by)
+    split_array = np.split(string_array, split_at)
 
-    # To convert the 1x3 arrays into 3x1 matrices, first check if all arrays are full (i.e., have 3 elements)
-    # If the last array is missing elements, find how many and add in the letter 'Z'(number 25) as a filler
-    leftoverDifference = 3 - len(splitarr[-1])
-    for i in range(leftoverDifference):
-        splitarr[-1] = np.append(splitarr[-1], [25])
+    # Check if the last 1x3 array is complete, and if not, add the letter 'Z' (index 25) to fill the remaining spaces
+    leftover_difference = 3 - len(split_array[-1])
+    for i in range(leftover_difference):
+        split_array[-1] = np.append(split_array[-1], [25])
+
+    return split_array, loop_range
+
+
+def encrypt(plaintext, key):
+    # Defining variables
+    ciphertext = ''
+    formatted_key = key_to_matrix(key)
+    formatted_plaintext, loop_range = string_to_matrix(plaintext)
 
     # Perform matrix multiplication and modulo 26 a number of times equal to the number of matrices
     for i in range(loop_range):
-        plainTextMatrix = splitarr[i].reshape(3, 1)
-        keyTextProduct = np.matmul(keyMatrix, plainTextMatrix)
-        cipherTextMatrix = keyTextProduct % 26
+        plaintext_matrix = formatted_plaintext[i].reshape(3, 1)
+        key_text_product = np.matmul(formatted_key, plaintext_matrix)
+        ciphertext_matrix = key_text_product % 26
 
         # Convert the resulting 3x1 matrix back into a 1x3 array
-        cipherTextArray = np.squeeze(cipherTextMatrix)
+        ciphertext_array = np.squeeze(ciphertext_matrix)
 
         # Convert the 1x3 array into cipher text
-        for i in cipherTextArray:
-            cipherText += f'{num_to_letter(int(i))}'
+        for j in ciphertext_array:
+            ciphertext += f'{num_to_letter(int(j))}'
 
-    return cipherText
+    return ciphertext
 
 
-def decrypt(cipherText, key):
+def decrypt(ciphertext, key):
     # Defining variables
-    keyArray = np.array([])
-    cipherTextArray = np.array([])
-    splitby = []
-    looprange = 1
-    plainText = ''
-
-    # Convert key to 3x3 matrix
-    for i in key:
-        keyArray = np.append(keyArray, [letter_to_num(i)])
-    keyMatrix = keyArray.reshape(3, 3)
+    plaintext = ''
+    formatted_key = key_to_matrix(key)
+    formatted_ciphertext, loop_range = string_to_matrix(ciphertext)
 
     # Fnd the determinant of the key matrix and its inverse modulo 26
-    modDet = int(np.linalg.det(keyMatrix) % 26)
-    invModDet = pow(modDet, -1, 26)
+    mod_det = int(np.linalg.det(formatted_key) % 26)
+    inv_mod_det = pow(mod_det, -1, 26)
 
     # Find the adjoint of the key matrix and take modulo 26
-    adj = (np.linalg.inv(keyMatrix) * np.linalg.det(keyMatrix)) % 26
+    adj = (np.linalg.inv(formatted_key) * np.linalg.det(formatted_key)) % 26
 
     # Find the inverse key matrix
-    inverseKeyMatrix = (invModDet * adj) % 26
-
-    # Convert cipher text to an array
-    for i in cipherText:
-        cipherTextArray = np.append(cipherTextArray, [letter_to_num(i)])
-
-    # Find the indices at which the cipher text array should be split to form 3x1 matrices
-    for i in range(1, len(cipherTextArray)):
-        if i % 3 == 0:
-            splitby.append(i)
-            looprange += 1
-
-    # Split the cipher text array into one or more 1x3 arrays
-    splitarr = np.split(cipherTextArray, splitby)
-
-    # To convert the 1x3 arrays into 3x1 matrices, first check if all arrays are full (i.e., have 3 elements)
-    # If the last array is missing elements, find how many and add in the letter 'Z'(number 25) as a filler
-    leftoverDifference = 3 - len(splitarr[-1])
-    for i in range(leftoverDifference):
-        splitarr[-1] = np.append(splitarr[-1], [25])
+    inverse_key_matrix = (inv_mod_det * adj) % 26
 
     # Perform matrix multiplication and modulo 26 a number of times equal to the number of matrices
-    for i in range(looprange):
-        cipherTextMatrix = splitarr[i].reshape(3, 1)
+    for i in range(loop_range):
+        ciphertext_matrix = formatted_ciphertext[i].reshape(3, 1)
+        key_text_product = np.matmul(inverse_key_matrix, ciphertext_matrix)
 
-        keyTextProduct = np.matmul(inverseKeyMatrix, cipherTextMatrix)
-
-        # Add 1 to the index because it kept returning 26 % 26 = 0, a hacky solution but it works
-        plainTextMatrix = ((keyTextProduct % 26)+0.5) % 26
+        # Add 0.5 to the index because it kept returning 26 % 26 = 0, a hacky solution, but it works
+        plaintext_matrix = ((key_text_product % 26) + 0.5) % 26
 
         # Convert the resulting 3x1 matrix back into a 1x3 array
-        plainTextArray = np.squeeze(plainTextMatrix)
+        plaintext_array = np.squeeze(plaintext_matrix)
 
         # Convert the 1x3 array into plain text
-        for i in plainTextArray:
-            plainText += f'{num_to_letter(int(i))}'
+        for j in plaintext_array:
+            plaintext += f'{num_to_letter(int(j))}'
 
-    return plainText
+    return plaintext
 
 
-encrypt('ILOVEYOU', 'GYBNQKURP')
-# Output: POH
-
-decrypt('OELMFQRYH', 'GYBNQKURP')
-# Output: ACT
+print(encrypt("HELLOWORLD", "GYBNQKURP"))
+print(decrypt("TFJIPIJSGTNC", "GYBNQKURP"))
