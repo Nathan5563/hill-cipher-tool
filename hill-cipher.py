b6bb4efece0e3beaae3dfd37f01fd20b595f690b
Nathan <ndawit611@gmail.com>
2024-02-25 12:52:23 +0300

    Check if key is a square matrix before calculating

--- hill-cipher.py
+++ hill-cipher.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 
 alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -23,11 +24,14 @@ def num_to_letter(num):
 def key_to_matrix(key):
     key_array = np.array([])
 
-    for i in key:
-        key_array = np.append(key_array, [letter_to_num(i)])
-    key_matrix = key_array.reshape(3, 3)
-
-    return key_matrix
+    # Check if key can be a square matrix
+    if int(math.sqrt(len(key))) == math.sqrt(len(key)):
+        for i in key:
+            key_array = np.append(key_array, [letter_to_num(i)])
+        key_matrix = key_array.reshape(3, 3)
+        return key_matrix
+    else:
+        return 0
 
 
 def string_to_matrix(string):
@@ -60,20 +64,23 @@ def encrypt(plaintext, key):
     formatted_key = key_to_matrix(key)
     formatted_plaintext, loop_range = string_to_matrix(plaintext)
 
-    # Perform matrix multiplication and modulo 26 a number of times equal to the number of matrices
-    for i in range(loop_range):
-        plaintext_matrix = formatted_plaintext[i].reshape(3, 1)
-        key_text_product = np.matmul(formatted_key, plaintext_matrix)
-        ciphertext_matrix = key_text_product % 26
+    if not isinstance(formatted_key, int):
+        # Perform matrix multiplication and modulo 26 a number of times equal to the number of matrices
+        for i in range(loop_range):
+            plaintext_matrix = formatted_plaintext[i].reshape(3, 1)
+            key_text_product = np.matmul(formatted_key, plaintext_matrix)
+            ciphertext_matrix = key_text_product % 26
 
-        # Convert the resulting 3x1 matrix back into a 1x3 array
-        ciphertext_array = np.squeeze(ciphertext_matrix)
+            # Convert the resulting 3x1 matrix back into a 1x3 array
+            ciphertext_array = np.squeeze(ciphertext_matrix)
 
-        # Convert the 1x3 array into cipher text
-        for j in ciphertext_array:
-            ciphertext += f'{num_to_letter(int(j))}'
+            # Convert the 1x3 array into cipher text
+            for j in ciphertext_array:
+                ciphertext += f'{num_to_letter(int(j))}'
 
-    return ciphertext
+        return ciphertext
+    else:
+        print("Invalid key: Must be a square matrix")
 
 
 def decrypt(ciphertext, key):
@@ -82,32 +89,35 @@ def decrypt(ciphertext, key):
     formatted_key = key_to_matrix(key)
     formatted_ciphertext, loop_range = string_to_matrix(ciphertext)
 
-    # Fnd the determinant of the key matrix and its inverse modulo 26
-    mod_det = int(np.linalg.det(formatted_key) % 26)
-    inv_mod_det = pow(mod_det, -1, 26)
+    if not isinstance(formatted_key, int):
+        # Fnd the determinant of the key matrix and its inverse modulo 26
+        mod_det = int(np.linalg.det(formatted_key) % 26)
+        inv_mod_det = pow(mod_det, -1, 26)
 
-    # Find the adjoint of the key matrix and take modulo 26
-    adj = (np.linalg.inv(formatted_key) * np.linalg.det(formatted_key)) % 26
+        # Find the adjoint of the key matrix and take modulo 26
+        adj = (np.linalg.inv(formatted_key) * np.linalg.det(formatted_key)) % 26
 
-    # Find the inverse key matrix
-    inverse_key_matrix = (inv_mod_det * adj) % 26
+        # Find the inverse key matrix
+        inverse_key_matrix = (inv_mod_det * adj) % 26
 
-    # Perform matrix multiplication and modulo 26 a number of times equal to the number of matrices
-    for i in range(loop_range):
-        ciphertext_matrix = formatted_ciphertext[i].reshape(3, 1)
-        key_text_product = np.matmul(inverse_key_matrix, ciphertext_matrix)
+        # Perform matrix multiplication and modulo 26 a number of times equal to the number of matrices
+        for i in range(loop_range):
+            ciphertext_matrix = formatted_ciphertext[i].reshape(3, 1)
+            key_text_product = np.matmul(inverse_key_matrix, ciphertext_matrix)
 
-        # Add 0.5 to the index because it kept returning 26 % 26 = 0, a hacky solution, but it works
-        plaintext_matrix = ((key_text_product % 26) + 0.5) % 26
+            # Add 0.5 to the index because it kept returning 26 % 26 = 0, a hacky solution, but it works
+            plaintext_matrix = ((key_text_product % 26) + 0.5) % 26
 
-        # Convert the resulting 3x1 matrix back into a 1x3 array
-        plaintext_array = np.squeeze(plaintext_matrix)
+            # Convert the resulting 3x1 matrix back into a 1x3 array
+            plaintext_array = np.squeeze(plaintext_matrix)
 
-        # Convert the 1x3 array into plain text
-        for j in plaintext_array:
-            plaintext += f'{num_to_letter(int(j))}'
+            # Convert the 1x3 array into plain text
+            for j in plaintext_array:
+                plaintext += f'{num_to_letter(int(j))}'
 
-    return plaintext
+        return plaintext
+    else:
+        print("Invalid key: Must be a square matrix")
 
 
 print(encrypt("HelloWorld", "GYBNQKURP"))
